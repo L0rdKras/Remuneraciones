a03afa4f53b974e34c5855bb08eeb506763de37a
Rodrigo Adones <rodrigo.adonesv@gmail.com>
2014-08-25 00:21:26 -0400

    Agregue la definicion del objeto empleado

--- Public/index.php
+++ Public/index.php
@@ -6,6 +6,7 @@ require "../Configuracion/helper.php";
 require "../Entidades/Consulta.php";
 require "../Entidades/OperacionesMySql.php";
 require "../Entidades/Empresa.php";
+require "../Entidades/Empleado.php";
 
 //Llamar al controlador indicado
 controller($_GET,$_POST);
